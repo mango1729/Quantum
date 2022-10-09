0e9dba05ce3959faab00a7bfe6510b0fa27e109f
manoj1729 <108476842+mango1729@users.noreply.github.com>
2022-10-09 14:56:51 +0530

    Create Simple_Interest.py

---
+++ Simple_Interest.py
@@ -0,0 +1,5 @@
+p=float(input("Enter the principle amount:"))
+t=int(input("Enter the time(years):"))
+r=float(input("Enter the rate:"))
+simple_interest=(p*t*r)/100
+print("The simple interest is:",simple_interest)
